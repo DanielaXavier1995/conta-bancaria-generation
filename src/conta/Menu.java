efb2244e3c23a61f1fe9b08c42d229cba1d38745
Daniela Xavier <danielamxsoares@gmail.com>
2023-12-14 16:31:11 -0300

    task 02 - criando o case 1 do menu

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -8,13 +8,11 @@ import conta.util.Cores;
 
 public class Menu {
 	
-	public static void exibirMenu() {
+	public static int exibirMenu(Scanner sc) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println(Cores.TEXT_PURPLE + Cores.ANSI_WHITE_BACKGROUND + "*********************************************");
+		System.out.println(Cores.TEXT_PURPLE + Cores.ANSI_WHITE_BACKGROUND + "*********************************************\n");
 		System.out.println("        BEM VINDO AO MENU DO BANCO GEN       ");
-		System.out.println("*********************************************");
+		System.out.println("\n*********************************************\n");
 		System.out.println("1 - ABRIR CONTA                              ");
 		System.out.println("2 - LISTAR TODAS AS CONTAS                   ");
 		System.out.println("3 - BUSCAR CONTA PELO NÚMERO                 ");
@@ -24,23 +22,82 @@ public class Menu {
 		System.out.println("7 - PARA TRANFERÊNCIA ENTRE CONTA            ");
 		System.out.println("8 - PARA FECHAR A CONTA                      ");
 		System.out.println("9 - PARA APAGAR A CONTA                      ");
-		System.out.println("10 - PARA SAIR DO SISTEMA                    ");
-		System.out.println("*********************************************");
+		System.out.println("0 - PARA SAIR DO SISTEMA                    ");
+		System.out.println("\n*********************************************");
 		System.out.print("Digite a opção desejada:                    ");
 		int op = sc.nextInt();
 		System.out.println("*********************************************" + Cores.TEXT_RESET);
 		
-		sc.close();
+		return op;
 		
 	}
 
 	public static void main(String[] args) {
 		
-	//Conta c1 = new Conta("123456", "01", "Daniela Xavier", Tipo.CONTA_CORRENTE, 1000.0f, true);
-
-	//c1.vizualizarInformaçõesDaConta();
+	Scanner sc = new Scanner(System.in);
 		
-	exibirMenu();
+	Conta conta = new Conta();
 
+	int opcao;
+	
+	do {
+
+	opcao = exibirMenu(sc);
+	
+	switch(opcao) {
+	
+	case 1:
+		System.out.println("*********************************************\n");
+		System.out.println("      ** BANCO GEN - SEJA SEU FUTURO!! **      ");
+		System.out.println("\n*********************************************");
+		System.out.print("Digite o número de conto (com 6 digitos): ");
+		conta.setNumero(sc.nextLine());
+		System.out.println("\n*********************************************");
+		System.out.print("Digite a agência (com 2 dígitos): ");
+		conta.setAgencia(sc.nextLine());
+		System.out.println("\n*********************************************");
+		System.out.print("Digite o nome do titular: ");
+		conta.setNomeDoTiular(sc.nextLine());
+		System.out.println("\n*********************************************");
+		System.out.println("Digite o saldo da conta: ");
+		conta.setSaldo(sc.nextFloat());
+		System.out.println("\n*********************************************");
+		System.out.println("                 TIPO DA CONTA                 ");
+		System.out.println("\n*********************************************");
+		System.out.println("               1 - Conta Corrente              ");
+		System.out.println("               2 - Conta Poupança              ");
+		System.out.println("               3 - Conta Salário               ");
+		System.out.println("               4 - Conta Universitária         ");
+		System.out.println("Digite o tipo da conta: ");
+		
+		
+		//Como pegar o tipoooo???
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 8:
+		break;
+	case 9:
+		break;
+	default:
+		System.out.println("*********************************************");
+		System.out.println("      Opção Inválida, escolha novamente!     ");
+		System.out.println("*********************************************");
+		break;
+    }
+		
+	} while(opcao != 0);
+		
+    sc.close();
 	}
 }
