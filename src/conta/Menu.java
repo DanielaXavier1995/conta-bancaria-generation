1db3b6f1e6b5bd824d8f8dca092169fef82e385c
Daniela Xavier <danielamxsoares@gmail.com>
2023-12-14 12:54:08 -0300

    task 02 - ajustar menu na classe menu

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -11,7 +11,17 @@ public class Menu {
 		System.out.println("        BEM VINDO AO MENU DO BANCO GEN       ");
 		System.out.println("*********************************************/n");
 		System.out.println("1 - ABRIR CONTA");
-		System.out.println();
+		System.out.println("2 - LISTAR TODAS AS CONTAS");
+		System.out.println("3 - BUSCAR CONTA PELO NÚMERO");
+		System.out.println("4 - ATUALIZAR DADOS DA CONTA");
+		System.out.println("5 - PARA REALIZAR DEPÓSITO");
+		System.out.println("6 - PARA REALIZAR SAQUE");
+		System.out.println("7 - PARA TRANFERÊNCIA ENTRE CONTA");
+		System.out.println("8 - PARA FECHAR A CONTA");
+		System.out.println("9 - PARA APAGAR A CONTA");
+		System.out.println("10 - PARA EXIBIR O MENU");
+		System.out.println("11 - PARA SAIR DO SISTEMA");
+		System.out.println("/n*********************************************");
 		
 	}
 
